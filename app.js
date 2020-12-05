8824ad274f8b40ef9b15a401c805901d5ba5b9ae
David <dyclee@umich.edu>
2020-12-05 04:05:29 -0600

    no origin

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(cors({origin}));
+app.use(cors());
 // app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
