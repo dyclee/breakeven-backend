568655d5d5f766d7d1d7619a92eecf7da48775ff
David <dyclee@umich.edu>
2021-01-05 16:57:14 -0600

    added balances to seeder data

--- db/seeders/20201027232836-demoUser.js
+++ db/seeders/20201027232836-demoUser.js
@@ -13,6 +13,7 @@ module.exports = {
         imageUrl: "https://i0.wp.com/www.repol.copl.ulaval.ca/wp-content/uploads/2019/01/default-user-icon.jpg",
         createdAt: new Date(),
         updatedAt: new Date(),
+        balance: 127.90,
       },
       {
         fullName: "Rita Smith",
@@ -22,6 +23,7 @@ module.exports = {
         imageUrl: "https://thumbs.dreamstime.com/z/business-woman-portrait-21792902.jpg",
         createdAt: new Date(),
         updatedAt: new Date(),
+        balance: 127.90,
       },
       {
         fullName: "Frank Richards",
@@ -31,6 +33,7 @@ module.exports = {
         imageUrl: "https://thumbs.dreamstime.com/z/satisfied-business-man-26326961.jpg",
         createdAt: new Date(),
         updatedAt: new Date(),
+        balance: 127.90,
       },
       {
         fullName: "Kevin Durand",
@@ -40,6 +43,7 @@ module.exports = {
         imageUrl: "https://empiresportsmedia.com/wp-content/uploads/2019/10/USATSI_13421096.jpg",
         createdAt: new Date(),
         updatedAt: new Date(),
+        balance: 127.90,
       },
       {
         fullName: "Damien Juliard",
@@ -49,6 +53,7 @@ module.exports = {
         imageUrl: "https://cdn.elitesportsny.com/wp-content/uploads/2017/09/GettyImages-620946142.jpg",
         createdAt: new Date(),
         updatedAt: new Date(),
+        balance: 127.90,
       },
       {
         fullName: "Jeremy Win",
@@ -58,6 +63,7 @@ module.exports = {
         imageUrl: "https://justrichest.com/wp-content/uploads/jeremy-lin-640x384.jpg",
         createdAt: new Date(),
         updatedAt: new Date(),
+        balance: 127.90,
       },
       {
         fullName: "Wardell Curry",
@@ -67,6 +73,7 @@ module.exports = {
         imageUrl: "https://www.bet.com/news/sports/2016/120/16/how-steph-curry-could-ink-deal-worth-over--200-million/_jcr_content/image.large2x1image.dimg/__1481902931795__1481898196313/121616-sports-steph-curry-smiles.jpg",
         createdAt: new Date(),
         updatedAt: new Date(),
+        balance: 127.90,
       },
       {
         fullName: "Bruce Barner",
@@ -76,6 +83,7 @@ module.exports = {
         imageUrl: "https://img3.looper.com/img/gallery/will-bruce-banner-be-in-the-disney-she-hulk-series/intro-1569264697.jpg",
         createdAt: new Date(),
         updatedAt: new Date(),
+        balance: 127.90,
       },
       {
         fullName: "Jennifer Talisman",
@@ -85,6 +93,7 @@ module.exports = {
         imageUrl: "https://1.bp.blogspot.com/-KM7Qtv9XTvA/VCXVcCm_AsI/AAAAAAAAf94/9aHgkh9XHC4/s3500/Jennifer%2BAniston%2Bphoto.filmcelebritiesactresses.blogspot-137.jpg",
         createdAt: new Date(),
         updatedAt: new Date(),
+        balance: 127.90,
       },
       {
         fullName: "Hannah Montini",
@@ -94,6 +103,7 @@ module.exports = {
         imageUrl: "https://akns-images.eonline.com/eol_images/Entire_Site/2016927/rs_1024x818-161027110450-1024.hannah-montana.102716.jpg",
         createdAt: new Date(),
         updatedAt: new Date(),
+        balance: 127.90,
       },
       {
         fullName: "Jim Halpert",
@@ -103,6 +113,7 @@ module.exports = {
         imageUrl: "https://1to10reviews.files.wordpress.com/2008/02/jim_office.png",
         createdAt: new Date(),
         updatedAt: new Date(),
+        balance: 127.90,
       },
       {
         fullName: "Michelle Orama",
@@ -112,6 +123,7 @@ module.exports = {
         imageUrl: "https://www.nowplayingnashville.com/wp-content/uploads/sites/www.nowplayingnashville.com/images/2018/12/Michelle-Obama-Nashville.jpg",
         createdAt: new Date(),
         updatedAt: new Date(),
+        balance: 127.90,
       },
       {
         fullName: "Robby Dinero",
@@ -121,6 +133,7 @@ module.exports = {
         imageUrl: "https://alongtheboards.com/wp-content/uploads/2019/10/1571334993_robert-de-niro-having-angry-meltdown-over-divorce-and-former-assistants-lawsuit.jpg",
         createdAt: new Date(),
         updatedAt: new Date(),
+        balance: 127.90,
       },
       {
         fullName: "Aqua Fina",
@@ -130,6 +143,7 @@ module.exports = {
         imageUrl: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.indiewire.com%2Fwp-content%2Fuploads%2F2018%2F11%2FAwkwafina.jpg",
         createdAt: new Date(),
         updatedAt: new Date(),
+        balance: 127.90,
       },
       {
         fullName: "Flamin Hots",
@@ -139,6 +153,7 @@ module.exports = {
         imageUrl: "https://media.syracuse.com/movies_impact/photo/cheetos.jpg",
         createdAt: new Date(),
         updatedAt: new Date(),
+        balance: 127.90,
       },
     ])
     /*
